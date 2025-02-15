86f99dd63975e59487be0866467168673166b8be
kyachingprue <kyachingpruemarma.info@gmail.com>
2025-02-15 09:54:57 +0600

    new commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+      <h1 className='text-center my-7 bg-yellow-700 text-white'>React Knowledge Projects</h1>
+
+    </>
+  )
+}
+
+export default App
